4e8a26d61da85157b4572017d8505abc0c63f3bf
Bashar <bashargh315@gmail.com>
2026-09-23 03:11:19 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ai-company-client-v1';
-const ASSETS=['./','./index.html','../shared/app.css','../shared/client.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
